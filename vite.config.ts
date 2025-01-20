4354912d284dab7a6dbab6577bc5e806df0f6c98
aysi <me@pebble.dog>
2025-01-20 12:41:38 -0800

    fix github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: 'THJ_PARSER'
-})
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+    return {
+        plugins: [react()],
+        envPrefix: 'THJ_LOGS',
+        base: command === `serve` ? `/` : `/thj-logs/`
+    }
+});
