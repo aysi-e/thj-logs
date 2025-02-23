f40fdb8846a0dacb38e2ca25980c77402fced7df
aysi <me@pebble.dog>
2025-02-22 22:04:06 -0800

    start some ui refactoring

--- src/state/encounter.ts
+++ src/state/encounter.ts
@@ -10,6 +10,27 @@ class EncounterState {
      */
     constructor(encounter: Encounter) {
         makeAutoObservable(this);
+
+        // for enemies/allies, we will need:
+        // - damage dealt by all enemies/allies, by target.
+        // - damage dealt by all enemies/allies, by damage type.
+        // - damage taken by all enemies/allies, by target.
+        // - damage taken by all enemies/allies, by damage type.
+        // - healing done by all enemies/allies, by target.
+        // - healing taken by all enemies/allies, by target.
+
+        // for each entity, we will need:
+        // - damage done to all targets by damage type.
+        // - damage done by target.
+        // - damage done to each target by damage type.
+
+        // - damage taken from all targets by damage type.
+        // - damage taken by target.
+        // - damage taken from each target by damage type.
+
+        // - damage taken from all targets by damage type.
+        // - damage taken by target.
+        // - damage taken from each target by damage type.
     }
 }
 
